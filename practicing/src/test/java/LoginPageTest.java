1ac88e5a9ba072231cf4195644e3a2e19026b651
Jose Penaherrera <Jose.Penaherrera@crowley.com>
2023-11-19 21:27:25 -0500

    adding the first test

---
+++ practicing/src/test/java/LoginPageTest.java
@@ -0,0 +1,38 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.RealWordLoginPage;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPageTest {
+    WebDriver driver;
+    RealWordLoginPage objRealWordLoginPage;
+    String titleName = "Cypress Real World App";
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        //ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new FirefoxDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://localhost:3000/signin");
+        String webTitle = driver.getTitle();
+        Assert.assertEquals(webTitle,titleName);
+    }
+
+    @Test(description = "Be able to login", priority = 0)
+    public void login(){
+        objRealWordLoginPage = new RealWordLoginPage(driver);
+        objRealWordLoginPage.enterUsername("Katharina_Bernier");
+        objRealWordLoginPage.enterPassword("s3cret");
+        objRealWordLoginPage.clickLoginButton();
+    }
+
+
+}
